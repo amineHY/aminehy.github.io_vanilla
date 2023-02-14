5c3d438cd8396bf75ac94f7b2acb11527432381c
Amine <hadjyoucef.amine@gmail.com>
2023-02-14 15:24:17 +0100

    correct theme and add articles

--- generate_html_files.sh
+++ generate_html_files.sh
@@ -1,5 +1,8 @@
-asciidoctor README.adoc -o /html/index.html
+asciidoctor README.adoc -o index.html
 asciidoctor contents/cv_fr.adoc -o html/cv_fr.html
 asciidoctor contents/cv_en.adoc -o html/cv_en.html
 asciidoctor contents/publication_fr.adoc -o html/publication_fr.html
 asciidoctor contents/publication_en.adoc -o html/publication_en.html
+asciidoctor contents/articles_fr.adoc -o html/articles_fr.html
+asciidoctor contents/blog/guide_ml.adoc -o html/blog/guide_ml.html
+
