de8649a65481e635e28018c754caca05d37f7b73
Amine <hadjyoucef.amine@gmail.com>
2023-02-14 10:52:39 +0100

    push yst changes after setting  new folder structure

--- generate_html_files.sh
+++ generate_html_files.sh
@@ -1,3 +1,5 @@
-asciidoctor README.adoc -o index.html 
-
-asciidoctor cv/cv_fr.adoc 
+asciidoctor README.adoc -o /html/index.html
+asciidoctor contents/cv_fr.adoc -o html/cv_fr.html
+asciidoctor contents/cv_en.adoc -o html/cv_en.html
+asciidoctor contents/publication_fr.adoc -o html/publication_fr.html
+asciidoctor contents/publication_en.adoc -o html/publication_en.html
