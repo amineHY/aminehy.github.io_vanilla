2d997d8571cb21c91433e351aa9befcc9efb643f
Amine <hadjyoucef.amine@gmail.com>
2023-02-17 01:40:04 +0100

    update content

--- common/meta/language_selector.js
+++ common/meta/language_selector.js
@@ -5,21 +5,34 @@ function changeLanguage() {
   const cvLink = document.querySelector(
     'a[href="/html/cv_fr.html"], a[href="/html/cv_en.html"]'
   );
-  const publicationLink = document.querySelector(
+  const publicationLink = document.querySelectorAll(
     'a[href="/html/publication_fr.html"], a[href="/html/publication_en.html"]'
   );
-  const indexLink = document.querySelector(
+  const indexLink = document.querySelectorAll(
     'a[href="index_fr.html"], a[href="index_en.html"]'
   );
 
-  console.log(language, cvLink, publicationLink, indexLink);
   if (language === "fr") {
     cvLink.href = "/html/cv_fr.html";
-    publicationLink.href = "/html/publication_fr.html";
-    indexLink.href = "index_fr.html";
+    publicationLink.forEach((link) => (link.href = "/html/publication_fr.html"));
+    indexLink.forEach((link) => (link.href = "index_fr.html"));
+    localStorage.setItem("selectedLanguage", "fr");
   } else {
     cvLink.href = "/html/cv_en.html";
-    publicationLink.href = "/html/publication_en.html";
-    indexLink.href = "index_en.html";
+    publicationLink.forEach((link) => (link.href = "/html/publication_en.html"));
+    indexLink.forEach((link) => (link.href = "index_en.html"));
+    localStorage.setItem("selectedLanguage", "en");
   }
 }
+
+window.addEventListener("load", () => {
+  const selectedLanguage = localStorage.getItem("selectedLanguage");
+  console.log(selectedLanguage)
+  if (selectedLanguage) {
+    const languageSelect = document.getElementById("language-select");
+    if (languageSelect) {
+      languageSelect.value = selectedLanguage;
+      changeLanguage();
+    }
+  }
+});
